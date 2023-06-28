7b3feed5cedda677148f0edb01a9915e841f15a8
candela99 <gonzalezcandela759@gmail.com>
2023-06-27 21:22:20 -0300

    create componente de tipo clase

--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 
 class Greeting extends Component {
 
+    myAge = 29;
+
     constructor(props) {
         super(props);
         this.state = {
-            age : 23
+            age : this.myAge
         }
     }
 
@@ -18,16 +20,29 @@ class Greeting extends Component {
                     ¡Hola {this.props.name}!
                 </h1>
                 <h2>Tu edad es de: {this.state.age}</h2>
+                <div>
+                    <button onClick={this.birthday}>
+                        Cumplir años
+                    </button>
+                </div>
             </div>
         );
+    }
+    
+    birthday = () => {
+        /*Si no uso el setState y hago solo this.myAge++, el valor de myAge cambiaría pero la vista no*/
+        this.setState((prevState) => ({
+            age: prevState.age +1
+        }))
     }
 }
 
 /*
 * Los propTypes son contenidos que puedo pasar a un componente de orden superior
+* Definen los tipos de datos que deben tener las props
 */
 Greeting.propTypes = {
-
+    name: PropTypes.string,
 };
 
 
